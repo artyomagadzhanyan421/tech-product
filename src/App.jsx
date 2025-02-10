a94274e8b79cf112e6bcdd8297d82668cb223c63
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-02-10 23:42:31 +0500

    block commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 // Components
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Features from './components/Features';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Features />
     </div>
   )
 }
---
+++ src/components/Features.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// CSS
+import "../styles/Features.css";
+
+function Features() {
+    return (
+        <div className='Block'>
+            <center><p className='subHead'>Features</p></center>
+            <center><p className="heading blockHeading">It’s packed with all you need</p></center>
+            <center>
+                <p className="desc blockDesc">Discover our features designed to make your workflow simpler, smarter, and more effective. Explore how we help you achieve more with ease.</p>
+            </center>
+        </div>
+    )
+}
+
+export default Features
