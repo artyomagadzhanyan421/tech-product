93b388f4160e97be5151af6cecd7114f328788fe
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-02-10 23:36:20 +0500

    hero commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 
 // Components
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   )
 }
